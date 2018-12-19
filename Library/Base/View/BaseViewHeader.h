80122d03ae42a74f12e1c5c632b78c9212b2a7ff
shenyb@xiaomodo.com <bin720097>
2018-12-19 15:33:03 +0800

    第三方库优化

--- Library/Base/View/BaseViewHeader.h
+++ Library/Base/View/BaseViewHeader.h
@@ -8,7 +8,6 @@
 #ifndef BaseViewHeader_h
 #define BaseViewHeader_h
 
-#import "UIImageView+WebImage.h"
 #import "UIButton+OnClick.h"
 #import "UIButton+EnlargeEdge.h"
 
---
+++ Library/SDK/LibrarySDKHeader.h
@@ -0,0 +1,13 @@
+//
+//  LibrarySDKHeader.h
+//  iProject
+//
+//  Created by BinGe on 2018/12/19.
+//
+
+#ifndef LibrarySDKHeader_h
+#define LibrarySDKHeader_h
+
+#import "UIImageView+WebImage.h"
+
+#endif /* LibrarySDKHeader_h */
