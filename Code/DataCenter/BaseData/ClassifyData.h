315b9e946eb9109bcd900aa1794db431ea771e90
shenyb@xiaomodo.com <bin720097>
2018-12-26 14:24:25 +0800

    添加分类数据

--- Code/DataCenter/BaseData/ClassifyData.h
+++ Code/DataCenter/BaseData/ClassifyData.h
@@ -11,6 +11,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ClassifyData : Data
 
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *image;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Code/DataCenter/Home/HomeData.m
+++ Code/DataCenter/Home/HomeData.m
@@ -19,6 +19,7 @@
 - (void)setData:(Data *)data {
     [super setData:data];
     
+    //banner
     NSFileManager *manager = [NSFileManager defaultManager];
     NSError *error;
     NSString *folder = [[NSBundle mainBundle] pathForResource:@"banner" ofType:nil];
@@ -28,6 +29,20 @@
         data.imageUrl = [NSString stringWithFormat:@"%@/%@", folder, name];
         [self.banner addObject:data.imageUrl];
     }
+    
+    //分类
+    NSArray *classify = @[@{@"name":@"hydrotherapy",@"title":@"水疗",@"image":@"@水疗"},
+                          @{@"name":@"Tuina",@"title":@"推拿",@"image":@"@推拿"},
+                          @{@"name":@"foot-bathing",@"title":@" 浴足",@"image":@"@浴足"}
+                          ];
+    for (NSDictionary *dic in classify) {
+        ClassifyData *data = [ClassifyData new];
+        data.name = [dic objectForKey:@"name"];
+        data.title = [dic objectForKey:@"title"];
+        data.image = [dic objectForKey:@"image"];
+        [self.classify addObject:data];
+    }
+    
 }
 
 @end
