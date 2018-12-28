d32693e8f0e68431ba31758b0154256cf69beae1
shenyb@xiaomodo.com <bin720097>
2018-12-28 11:23:15 +0800

    添加orderController

---
+++ Code/Module/Main/Order/OrderViewController.h
@@ -0,0 +1,16 @@
+//
+//  OrderViewController.h
+//  APP
+//
+//  Created by BinGe on 2018/12/28.
+//
+
+#import "MainViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OrderViewController : MainViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
