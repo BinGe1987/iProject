8f940942d2145ec007e99b35f6b94ce04afcb8d7
shenyb@xiaomodo.com <bin720097>
2019-01-02 12:16:47 +0800

    navigation bar 去返回按钮文字

--- Code/Module/Base/Controller/BaseNavigationController.m
+++ Code/Module/Base/Controller/BaseNavigationController.m
@@ -15,12 +15,15 @@
     self.navigationBar.translucent = NO;
     [self.navigationBar setBackgroundImage:[ImageUtils imageWithColorHex:@"#ffffffff" size:CGSizeMake(10, 10)] forBarMetrics:UIBarMetricsDefault];
     [self.navigationBar setShadowImage:[UIImage new]];
+    
+    [self.navigationBar setTintColor:UIColorMain];
+    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor clearColor]}forState:UIControlStateNormal];
+
 }
 
 -(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
     viewController.hidesBottomBarWhenPushed = YES;
     [super pushViewController:viewController animated:animated];
 }
-
 
 @end
