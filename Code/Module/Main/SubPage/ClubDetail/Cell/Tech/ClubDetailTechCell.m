fdf2f3ad93b2c72fc0d5b6922b76ccb42c9b5d8e
shenyb@xiaomodo.com <bin720097>
2019-01-09 13:57:30 +0800

    会所详情 技师cell

--- Code/Module/Main/SubPage/ClubDetail/Cell/Tech/ClubDetailTechCell.m
+++ Code/Module/Main/SubPage/ClubDetail/Cell/Tech/ClubDetailTechCell.m
@@ -36,15 +36,25 @@
 }
 
 - (void)setData:(id)data {
+    NSArray *techList = data;
+    NSInteger count = techList.count > 4 ? 4 : techList.count;
     LinearLayout *linearView = (LinearLayout *)[self.contentView findViewByName:@"linear"];
-//    CGSize size = CGSizeMake(ScaleValue(84), linearView.height);
-    for (UIView *chidView in linearView.subviews) {
-        UIButton *btn = (UIButton *)[chidView findViewByName:@"btn"];
+    for (int i=0; i<count; i++) {
+        ViewGroup *item = linearView.subviews[i];
+        TechData *tech = [techList[i] copy];
+        
+        UIImageView *imageView = [item findViewByName:@"image"];
+        [imageView setImageWithURL:[NSURL URLWithString:tech.imageUrl] placeholder:UIImageDefault_Tech];
+        
+        UILabel *label = [item findViewByName:@"name"];
+        label.text = tech.name;
+        
+        UIButton *btn = (UIButton *)[item findViewByName:@"btn"];
         [btn setClickBlock:^(UIButton * _Nonnull button) {
-            [UIViewController pushController:@"TechDetailController" animated:YES data:nil];
+            [UIViewController pushController:@"TechDetailController" animated:YES data:tech];
         }];
     }
-    [linearView onLayout];
+    [linearView refreshLaoyout];
 }
 
 @end
